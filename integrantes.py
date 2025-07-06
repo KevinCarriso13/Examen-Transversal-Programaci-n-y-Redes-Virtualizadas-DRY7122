ff48491aab6cef77cfb1ae0282bbef765b56205c
KevinCarriso13 <kevincarrisop@gmail.com>
2025-07-06 02:20:53 +0000

    Change Cisco to NetAcad

---
+++ integrantes.py
@@ -0,0 +1,6 @@
+# Definir la lista de nombres y apellidos (lista manual)
+miembros_grupo = ["Kevin Carriso", "Jean Pierre"]
+
+# Recorrer la lista e imprimir los nombres y apellidos
+for miembro in miembros_grupo:
+    print(miembro)
